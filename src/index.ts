6c2cfc8e383f8130e6a95bf6d13df004305011a3
xinshangshangxin <xinshangshangxin@gmail.com>
2018-09-25 17:31:32 +0800

    feat: add default queue name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import bb from 'bluebird';
 import Queue from 'amqp-queue';
 
 export default function lift() {
-  let queue = new Queue(this.config.connections.queue.name, this.config.connections.queue);
+  let queue = new Queue(this.config.connections.queue.name || 'mlop-queue', this.config.connections.queue);
 
   queue.createAndSave = function(name, data) {
     let job = queue.create(name, data);
